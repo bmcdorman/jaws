a5a05018beba4cc489f87c3ad21df6dfd0b802f4
Braden McDorman <bmcdorman@gmail.com>
2013-07-01 01:11:40 -0500

    Added message tests

---
+++ src/test/java/com/github/jaws/proto/MessageTest.java
@@ -0,0 +1,42 @@
+package com.github.jaws.proto;
+
+import com.github.jaws.util.RandomData;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Braden McDorman
+ */
+@RunWith(JUnit4.class)
+public class MessageTest {
+	@Test
+	public void dataRetained() {
+		final Message m = new Message();
+		final byte[] data = RandomData.getByteArray(213);
+		m.setData(data);
+		
+		assertEquals("data not retained", data, m.getData());
+	}
+	
+	@Test
+	public void typeRetained() {
+		final Message m = new Message();
+		m.setType(Message.Type.Ping);
+		
+		assertEquals("type not retained", Message.Type.Ping, m.getType());
+	}
+	
+	@Test
+	public void freshlyConstructed() {
+		final Message m = new Message();
+		assertEquals("non-invalid type", Message.Type.Invalid, m.getType());
+		assertNull(m.getData());
+	}
+}
