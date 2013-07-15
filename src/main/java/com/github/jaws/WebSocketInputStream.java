74f36c00dde3d0d947202328f2b4184c5d5d343e
Braden McDorman <bmcdorman@gmail.com>
2013-07-15 04:54:54 -0500

    Completed the logic for exposing the message based websocket protocol as java streams. Your stream is converted into a websocket message, then a websocket frame, and then a stream again...

--- src/main/java/com/github/jaws/WebSocketInputStream.java
+++ src/main/java/com/github/jaws/WebSocketInputStream.java
@@ -1,14 +1,71 @@
 package com.github.jaws;
 
+import com.github.jaws.proto.Message;
+import com.github.jaws.util.ResizableCircularByteBuffer;
+
+import java.io.IOException;
 import java.io.InputStream;
 
 /**
- *
+ * WebSocketInputStream turns the discrete message protocol of WebSockets into a
+ * normal input stream. This is meant to be used in conjunction with the jaws-specific
+ * WebSocket transparency protocol and package.
+ * 
  * @author Braden McDorman
  */
 public class WebSocketInputStream extends InputStream {
-	@Override
-	public int read() {
-		return -1;
+	private boolean autoPolling;
+	private WebSocket webSocket;
+	private ResizableCircularByteBuffer incoming;
+	
+	WebSocketInputStream(final WebSocket webSocket) {
+		this.webSocket = webSocket;
 	}
+	
+	public void setAutoPolling(final boolean autoPolling) {
+		this.autoPolling = autoPolling;
+	}
+	
+	public boolean isAutoPolling() {
+		return autoPolling;
+	}
+
+	private void autoPoll() throws WebSocketException, IOException {
+		if(!autoPolling) return;
+		webSocket.poll();
+	}
+	
+	private void ensureData(final int length) throws WebSocketException {
+		if(incoming.available() >= length) return;
+		Message m = null;
+		while(incoming.available() < length && (m = webSocket.recv()) != null) {
+			incoming.write(m.getData(), 0, m.getDataLength());
+		}
+	}
+	
+	@Override
+	public int read() throws IOException {
+		try {
+			autoPoll();
+			ensureData(1);
+		} catch(WebSocketException e) {
+			throw new IOException(e.getCause());
+		}
+		
+		return incoming.read();
+	}
+	
+	@Override
+	public int read(byte[] buffer, int offset, int length) throws IOException {
+		try {
+			autoPoll();
+			ensureData(length);
+		} catch(WebSocketException e) {
+			throw new IOException(e.getCause());
+		}
+		
+		return incoming.read(buffer, offset, length);
+	}
+	
+	
 }
--- src/main/java/com/github/jaws/WebSocketOutputStream.java
+++ src/main/java/com/github/jaws/WebSocketOutputStream.java
@@ -1,21 +1,60 @@
 package com.github.jaws;
 
-import com.github.jaws.proto.OutgoingStreamProcessor;
+import com.github.jaws.util.ResizableCircularByteBuffer;
+
+import java.io.IOException;
 import java.io.OutputStream;
 
 /**
- *
+ * WebSocketOutputStream turns the discrete message protocol of WebSockets into a
+ * normal output stream. This is meant to be used in conjunction with the jaws-specific
+ * WebSocket transparency protocol and package.
+ * 
  * @author Braden McDorman
  */
 public class WebSocketOutputStream extends OutputStream {
-	private OutputStream parent;
+	private boolean autoPolling;
+	private WebSocket webSocket;
+	private ResizableCircularByteBuffer outgoing;
 	
-	public WebSocketOutputStream(final OutputStream parent, final OutgoingStreamProcessor out) {
-		this.parent = parent;
+	WebSocketOutputStream(final WebSocket webSocket) {
+		this.webSocket = webSocket;
+	}
+	
+	public void setAutoPolling(final boolean autoPolling) {
+		this.autoPolling = autoPolling;
+	}
+	
+	public boolean isAutoPolling() {
+		return autoPolling;
+	}
+
+	private void autoPoll() throws WebSocketException, IOException {
+		if(!autoPolling) return;
+		webSocket.poll();
 	}
 	
 	@Override
-	public void write(int b) {
+	public void write(int b) throws IOException {
+		outgoing.write((byte)(b & 0xFF));
 		
+		try {
+			autoPoll();
+		} catch(WebSocketException e) {
+			throw new IOException(e.getCause());
+		}
 	}
+	
+	@Override
+	public void write(byte[] buffer, int offset, int length) throws IOException {
+		outgoing.write(buffer, offset, length);
+		
+		try {
+			autoPoll();
+		} catch(WebSocketException e) {
+			throw new IOException(e.getCause());
+		}
+	}
+	
+	
 }
